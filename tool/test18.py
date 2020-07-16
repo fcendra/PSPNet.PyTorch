8c380674ba3fdd450eefadabc68b73b50b07143a
Fernando Julio Cendra <fcendra12@gmail.com>
2020-07-16 19:25:58 +0800

    Refactor test Scripts #3

--- tool/test18.py
+++ tool/test18.py
@@ -195,8 +195,39 @@ def net_process(model, image, mean, std=None, flip=True):
     return output
 
 
-def test(test_loader, data_list, model, gray_folder,
-         colors,color_folder):
+def scale_process(model, image, classes, crop_h, crop_w, h, w, mean, std=None, stride_rate=2/3):
+    ori_h, ori_w, _ = image.shape
+    pad_h = max(crop_h - ori_h, 0)
+    pad_w = max(crop_w - ori_w, 0)
+    pad_h_half = int(pad_h / 2)
+    pad_w_half = int(pad_w / 2)
+    if pad_h > 0 or pad_w > 0:
+        image = cv2.copyMakeBorder(image, pad_h_half, pad_h - pad_h_half, pad_w_half, pad_w - pad_w_half, cv2.BORDER_CONSTANT, value=mean)
+    new_h, new_w, _ = image.shape
+    stride_h = int(np.ceil(crop_h*stride_rate))
+    stride_w = int(np.ceil(crop_w*stride_rate))
+    grid_h = int(np.ceil(float(new_h-crop_h)/stride_h) + 1)
+    grid_w = int(np.ceil(float(new_w-crop_w)/stride_w) + 1)
+    prediction_crop = np.zeros((new_h, new_w, classes), dtype=float)
+    count_crop = np.zeros((new_h, new_w), dtype=float)
+    for index_h in range(0, grid_h):
+        for index_w in range(0, grid_w):
+            s_h = index_h * stride_h
+            e_h = min(s_h + crop_h, new_h)
+            s_h = e_h - crop_h
+            s_w = index_w * stride_w
+            e_w = min(s_w + crop_w, new_w)
+            s_w = e_w - crop_w
+            image_crop = image[s_h:e_h, s_w:e_w].copy()
+            count_crop[s_h:e_h, s_w:e_w] += 1
+            prediction_crop[s_h:e_h, s_w:e_w, :] += net_process(model, image_crop, mean, std)
+    prediction_crop /= np.expand_dims(count_crop, 2)
+    prediction_crop = prediction_crop[pad_h_half:pad_h_half+ori_h, pad_w_half:pad_w_half+ori_w]
+    prediction = cv2.resize(prediction_crop, (w, h), interpolation=cv2.INTER_LINEAR)
+    return prediction
+
+
+def test(test_loader, data_list, model, classes, mean, std, base_size, crop_h, crop_w, scales, gray_folder, color_folder, colors):
     logger.info('>>>>>>>>>>>>>>>> Start Evaluation >>>>>>>>>>>>>>>>')
     data_time = AverageMeter()
     batch_time = AverageMeter()
@@ -204,9 +235,22 @@ def test(test_loader, data_list, model, gray_folder,
     end = time.time()
     for i, (input, _) in enumerate(test_loader):
         data_time.update(time.time() - end)
-        prediction = predict_whole_img(model, input)
-        # prediction=prediction[0].numpy()
-        prediction = np.argmax(prediction, axis=3)
+        input = np.squeeze(input.numpy(), axis=0)
+        image = np.transpose(input, (1, 2, 0))
+        h, w, _ = image.shape
+        prediction = np.zeros((h, w, classes), dtype=float)
+        for scale in scales:
+            long_size = round(scale * base_size)
+            new_h = long_size
+            new_w = long_size
+            if h > w:
+                new_w = round(long_size/float(h)*w)
+            else:
+                new_h = round(long_size/float(w)*h)
+            image_scale = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+            prediction += scale_process(model, image_scale, classes, crop_h, crop_w, h, w, mean, std)
+        prediction /= len(scales)
+        prediction = np.argmax(prediction, axis=2)
         batch_time.update(time.time() - end)
         end = time.time()
         if ((i + 1) % 10 == 0) or (i + 1 == len(test_loader)):
@@ -215,19 +259,16 @@ def test(test_loader, data_list, model, gray_folder,
                         'Batch {batch_time.val:.3f} ({batch_time.avg:.3f}).'.format(i + 1, len(test_loader),
                                                                                     data_time=data_time,
                                                                                     batch_time=batch_time))
-
-        for g in range(0, prediction.shape[0]):
-            check_makedirs(gray_folder)
-            check_makedirs(color_folder)
-            gray = np.uint8(prediction[g])
-            color = colorize(gray, colors)
-
-            image_path, _ = data_list[i]
-            image_name = image_path.split('/')[-1].split('.')[0]
-            gray_path = os.path.join(gray_folder, image_name + '.png')
-            cv2.imwrite(gray_path, gray)
-            color_path = os.path.join(color_folder, image_name + '.png')
-            color.save(color_path)
+        check_makedirs(gray_folder)
+        check_makedirs(color_folder)
+        gray = np.uint8(prediction)
+        color = colorize(gray, colors)
+        image_path, _ = data_list[i]
+        image_name = image_path.split('/')[-1].split('.')[0]
+        gray_path = os.path.join(gray_folder, image_name + '.png')
+        color_path = os.path.join(color_folder, image_name + '.png')
+        cv2.imwrite(gray_path, gray)
+        color.save(color_path)
     logger.info('<<<<<<<<<<<<<<<<< End Evaluation <<<<<<<<<<<<<<<<<')
 
 
