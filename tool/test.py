8ee46589a2173c616d24c635be4a92e5ea2d25e5
Fernando Julio <fcendra@slurm-q01-inst-1.gpufarm.cs.hku.hk>
2020-07-19 11:22:24 +0800

    Added PSPNet50 for ADE20K model

--- tool/test.py
+++ tool/test.py
@@ -113,12 +113,12 @@ def main():
         if os.path.isfile(args.model_path):
             logger.info("=> loading checkpoint '{}'".format(args.model_path))
             checkpoint = torch.load(args.model_path)
-            #model.load_state_dict(checkpoint['state_dict'], strict=False)
+            model.load_state_dict(checkpoint['state_dict'], strict=False)
             # My code
-            state_dict = model.state_dict()
-            for k1, k2 in zip(state_dict.keys(), checkpoint.keys()):
-                state_dict[k1] = checkpoint[k2]
-            model.load_state_dict(state_dict)
+            #state_dict = model.state_dict()
+            #for k1, k2 in zip(state_dict.keys(), checkpoint.keys()):
+            #    state_dict[k1] = checkpoint[k2]
+            #model.load_state_dict(state_dict)
             # My code
             logger.info("=> loaded checkpoint '{}'".format(args.model_path))
         else:
--- tool/train.sh
+++ tool/train.sh
@@ -20,13 +20,13 @@ config=config/${dataset}/${dataset}_${exp_name}.yaml
 now=$(date +"%Y%m%d_%H%M%S")
 
 mkdir -p ${model_dir} ${result_dir}
-cp tool/train.sh tool/train.py tool/test.sh tool/test18.py ${config} ${exp_dir}
+cp tool/train.sh tool/train.py tool/test.sh tool/test.py ${config} ${exp_dir}
 
 export PYTHONPATH=./
 $PYTHON -u ${exp_dir}/train.py \
   --config=${config} \
   2>&1 | tee ${model_dir}/train-$now.log
 
-$PYTHON -u ${exp_dir}/test18.py \
+$PYTHON -u ${exp_dir}/test.py \
   --config=${config} \
   2>&1 | tee ${result_dir}/test-$now.log
