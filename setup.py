4c42d4fee399b225fa81cc2babefb67b3fe5785b
Fernando Julio Cendra <fcendra12@gmail.com>
2020-08-03 17:13:04 +0800

    Waiting for review from Mr. Dai

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from os.path import isfile, join
 from path import Path
 import numpy as np
 import cv2
-
+import json
 # Dataset path
-target_path = Path('target_val/')
-annotation_images_path = Path('dataset/ade20k/annotations/validation/').abspath()
+target_path = Path('target_train/')
+annotation_images_path = Path('dataset/ade20k/annotations/training/').abspath()
 dataset = [ f for f in listdir(annotation_images_path) if isfile(join(annotation_images_path,f))]
 images = np.empty(len(dataset), dtype = object)
 count = 1
 # Iterate all Training Images
 for n in range(0, len(dataset)):
     # Read image
-    images[n] = cv2.imread(join(annotation_images_path,dataset[n]))
-
+    images[n] = cv2.imread(join(annotation_images_path,dataset[n]),-1)
+ 
     # Convert it to array
     array = np.asarray(images[n],dtype=np.int8)
 
@@ -22,7 +22,6 @@ for n in range(0, len(dataset)):
     # If it is >= 1, increment it by -1
     arr = np.where(array < 1, 255, array -1)
 
-    #Saved it to another file 
     if count < 10:
         cv2.imwrite(target_path +'ADE_val_0000000'+ str(count) + ".png", arr)
     elif count < 100 and count > 9:
@@ -35,6 +34,9 @@ for n in range(0, len(dataset)):
         cv2.imwrite(target_path +'ADE_val_000'+ str(count) + ".png", arr)
     print(str(count) + ".png is printed")
     count += 1
-    
+    print(count)
+  
+
+
 
                 
