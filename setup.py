0b66a3374d425d1209b5f0028f5540388c30335a
Fernando Julio Cendra <fcendra12@gmail.com>
2020-08-05 14:50:04 +0800

    Fixed the image processing script

--- setup.py
+++ setup.py
@@ -5,23 +5,25 @@ import numpy as np
 import cv2
 import json
 # Dataset path
-target_path = Path('target_train/')
-annotation_images_path = Path('dataset/ade20k/annotations/training/').abspath()
+target_path = Path('target_val/')
+annotation_images_path = Path('dataset/ade20k/annotations/validation/').abspath()
 dataset = [ f for f in listdir(annotation_images_path) if isfile(join(annotation_images_path,f))]
 images = np.empty(len(dataset), dtype = object)
 count = 1
+
 # Iterate all Training Images
-for n in range(0, len(dataset)):
+
     # Read image
     images[n] = cv2.imread(join(annotation_images_path,dataset[n]),-1)
  
     # Convert it to array
-    array = np.asarray(images[n],dtype=np.int8)
+    array = np.asarray(images[n],dtype="uint8")
 
     # Conditions when the value equal less than 1, change it to 255. 
     # If it is >= 1, increment it by -1
     arr = np.where(array < 1, 255, array -1)
-
+    #counter = np.count_nonzero(arr < 0)
+    
     if count < 10:
         cv2.imwrite(target_path +'ADE_val_0000000'+ str(count) + ".png", arr)
     elif count < 100 and count > 9:
@@ -32,11 +34,6 @@ for n in range(0, len(dataset)):
         cv2.imwrite(target_path +'ADE_val_0000'+ str(count) + ".png", arr)
     else:
         cv2.imwrite(target_path +'ADE_val_000'+ str(count) + ".png", arr)
+
     print(str(count) + ".png is printed")
     count += 1
-    print(count)
-  
-
-
-
-                
